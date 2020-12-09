9d6a9037cc1599d287be0d4b8d3bfd5664d7f512
ijounjalkar <jounjalkarisha@gmail.com>
2020-12-09 20:58:40 +0530

    Added functionality of validation of name

--- target/generated-sources/annotations/com/capgemini/firstservlet/LoginServlet.java
+++ target/generated-sources/annotations/com/capgemini/firstservlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.capgemini.firstservlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -39,5 +41,17 @@ public class LoginServlet extends HttpServlet {
 			rd.include(request, response);
 		}
 	}
+	private boolean validateUserName(String name) {
+		String regex = "^([A-Z][A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(name);
+		if (matcher.find()) {
+			return true;
+		}
+		return false;
+	}
+
+
+
 
 }
